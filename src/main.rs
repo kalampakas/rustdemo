99d6de21c55c4603ddb23fe48df4ddcde30722ac
Vasileios Kalampakas <stoneforger@gmail.com>
2019-02-09 21:20:36 +0100

    guess game chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println! {"guess the number!"};
+    println! {"Input guess from 1 to 101"};
+    loop {
+        let secret_number = rand::thread_rng().gen_range(1, 101);
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Couldn't read line");
+        let f_guess = guess.trim_right().len();
+        guess.truncate(f_guess);
+        println!(
+            "You guessed: {} and the secret number was {}",
+            guess, secret_number
+        );
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Right on the money!");
+                break;
+                }
+        }
+    }
 }
